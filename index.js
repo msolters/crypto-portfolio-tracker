579f2d5c8a4efbadfac2616f72772249a30ac6c2
Mark Solters <msolters@gmail.com>
2017-12-21 21:33:14 -0500

    Sort and display each coin in portfolio

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const request = require('request')
+const _ = require('underscore')
 
 let holdings = JSON.parse( fs.readFileSync("holdings.json", "utf8") )
 
@@ -16,22 +17,23 @@ function get_coin( coin ) {
 
 function compute_portfolio() {
   return new Promise( (resolve, reject) => {
-    let coin_count = 0
+    let coins = {}
     let total_coins = Object.keys(holdings.crypto).length
     let total_portfolio_value = 0
     for( let coin in holdings.crypto ) {
       get_coin(coin)
       .then( (coin_data) => {
+        coins[coin_data.symbol] = coin_data
         let coin_value = parseFloat(coin_data.price_usd) * holdings.crypto[coin]
+        coins[coin_data.symbol].coin_value = coin_value
         total_portfolio_value += coin_value
-        //console.log(`${coin_data.symbol}:\t\t$${coin_value}`)
-        coin_count++
-        if( coin_count === total_coins ) {
+        if( Object.keys(coins).length === total_coins ) {
           let return_value = total_portfolio_value - holdings.invested
           resolve({
             total: total_portfolio_value,
             return: return_value,
             performance: (return_value / holdings.invested * 100),
+            coins
           })
         }
       })
@@ -45,7 +47,10 @@ function compute_portfolio() {
 setInterval( () => {
   compute_portfolio()
   .then( (portfolio) => {
-    console.log(`Return:\t\t$${portfolio.return.toFixed(2)}\t(${portfolio.performance.toFixed(2)}%)`)
+    for( let coin of _.sortBy(portfolio.coins, c => c.coin_value) ) {
+      console.log(`${coin.symbol}:\t\t$${coin.coin_value.toFixed(2)}`)
+    }
+    console.log(`Return:\t\t$${portfolio.return.toFixed(2)}\t(${portfolio.performance.toFixed(2)}%)\n`)
   })
   .catch( (error) => {
     console.log(`Encountered error: `, error)
